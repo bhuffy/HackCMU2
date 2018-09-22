8dc6675150ca809d6f2cf9c0d73888b9439db380
Bennett Huffman <bennett.w.huffman@gmail.com>
2018-09-22 05:02:44 -0400

    first commit

---
+++ constants/Colors.js
@@ -0,0 +1,14 @@
+const tintColor = '#ADEFDE';
+
+export default {
+  tintColor,
+  tabIconDefault: '#ADEFDE',
+  tabIconSelected: tintColor,
+  tabBar: '#fefefe',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+};
---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import MeScreen from '../screens/MeScreen';
+import ShareScreen from '../screens/ShareScreen';
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  ),
+};
+
+const MeStack = createStackNavigator({
+  Me: MeScreen,
+});
+
+MeStack.navigationOptions = {
+  tabBarLabel: 'Me',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? `ios-link${focused ? '' : '-outline'}` : 'md-link'}
+    />
+  ),
+};
+
+const ShareStack = createStackNavigator({
+  Share: ShareScreen,
+});
+
+ShareStack.navigationOptions = {
+  tabBarLabel: 'Shared',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? `ios-options${focused ? '' : '-outline'}` : 'md-options'}
+    />
+  ),
+};
+
+export default createBottomTabNavigator({
+  HomeStack,
+  MeStack,
+  ShareStack,
+});
